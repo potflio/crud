b17e06aa3a0664bf415795fbfb2481ed94a41f27
prakash <creativetechwithus@gmail.com>
2023-11-24 10:05:33 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+class Member(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "student_name"
---
+++ project/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('create/',views.create, name='create'),
+    path('read/',views.read, name='read'),
+    path('edit/<id>',views.edit, name='edit'),
+    path('edit/update/<id>',views.update, name='update'),
+    path('delete/<id>',views.delete, name='delete'),
+]
